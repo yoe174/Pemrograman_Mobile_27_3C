00061ab140801a27a7a7c0eaf344b3fbda2bf6ac
Yonatan Efrassetyo <117373364+yoe174@users.noreply.github.com>
2024-09-13 00:26:22 +0700

    Add files via upload

---
+++ P3_Praktikum 2.dart
@@ -0,0 +1,17 @@
+// langkah 1 - 2
+// void main(){
+// int counter = 0;
+//   while (counter < 33) {
+//     print(counter);
+//     counter++;
+//   }
+// }
+
+// langkah 3
+void main(){
+  int counter = 0;
+  do {
+  print(counter);
+  counter++;
+} while (counter < 77);
+}
