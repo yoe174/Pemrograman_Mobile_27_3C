0e4c68d7850b406047784c5e88cba06f67606fc8
Yonatan Efrassetyo <117373364+yoe174@users.noreply.github.com>
2024-09-13 00:03:12 +0700

    Add files via upload
    #3 praktikum1

---
+++ P3_Praktikum 1.dart
@@ -0,0 +1,24 @@
+// langkah 1-2
+// void main(){
+//   String test = "test1";
+//   if (test == "test1") {
+//     print("Test1");
+//   } else if (test == "test2")
+//     print("Test2");
+//     else{
+//       print("Something else");
+//     }
+
+//     if (test == "test2") {
+//       print("Test2 again");
+//     }
+// }
+
+// langkah 3
+void main(){
+  bool test = true;
+
+  if (test) {
+    print("Kebenaran");
+  }
+}
